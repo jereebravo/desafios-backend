38d38127e66e79e0db54e94768fa4c86b1b49d86
jeremias <jeremiasbravo44@gmail.com>
2023-12-28 23:17:41 -0300

    termino 2da pre entrega bravo jeremias

--- primerDesafio.js
+++ primerDesafio.js
@@ -1,7 +1,10 @@
+const fs = require("fs");
+
 class ProductManager {
   constructor(products = []) {
     this.products = products;
     this.ProductId = 1;
+    this.path = "./product.JSON";
   }
 
   addProduct(title, description, price, thumbnail, code, stock) {
@@ -24,33 +27,101 @@ class ProductManager {
     }
   }
 
-  getProducts(){
-    return this.products
+  getProducts() {
+    return this.products;
   }
 
-  getProductsById(id){
-    return this.products.find((product) => product.id === id);
+  getProductsFs() {
+    fs.promises
+      .readFile(this.path, "utf-8")
+      .then((res) => {
+        let productString = JSON.parse(res);
+        console.log(productString);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  getProductsById(id) {
+    fs.promises.readFile(this.path, "utf-8")
+      .then((res) => {
+        let products = JSON.parse(res);
+        let productId = products.find((product) => product.id === id);
+        console.log(productId);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  async updateProduct(id, contentUpdated) {
+    try {
+      const content = await fs.promises.readFile(this.path, "utf-8");
+      const products = JSON.parse(content);
+
+      const productIndex = products.findIndex((product) => product.id === id);
+
+      if (productIndex !== -1) {
+        Object.assign(products[productIndex], contentUpdated);
+
+        await fs.promises.writeFile(this.path,JSON.stringify(products, null, 2),"utf-8");
+
+        console.log(`Producto con ID ${id} actualizado exitosamente.`);
+      } else {
+        console.log(`Producto con ID ${id} no encontrado.`);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
+
+  saveProductsInFs() {
+    const content = JSON.stringify(product.getProducts(), null, 2);
+    try {
+      fs.promises.writeFile("product.JSON", content, "utf-8");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async deleteProduct(id){
+
+    try{
+      const content = await fs.promises.readFile(this.path, 'utf-8');
+    const products = JSON.parse(content);
+
+    const productId = products.findIndex((product) => product.id === id);
+
+    if(productId){
+      products.splice(productId, 1);
+      await fs.promises.writeFile(this.path, JSON.stringify(products, null , 2), 'utf-8');
+      console.log(`Producto con id: ${id} eliminado exitosamente.`)
+
+    }
+    else{
+      console.log(`El producto con el id: ${id} no fue encontrado`)
+    }
+
+    }
+    catch(err){
+      console.log("A ocurrido un error: ", err);
+    }
+
   }
 }
 
 const product = new ProductManager();
 
-product.addProduct("Producto 1" , "producto de prueba" , 200 ,"sin imagen" , "abc1" , 4);
-product.addProduct("Producto 2" , "producto de prueba" , 300 ,"sin imagen" , "abc2" , 8);
-product.addProduct("Producto 3" , "producto de prueba" , 400 ,"sin imagen" , "abc3" , 6);
+product.addProduct("libro", "libro de harry potter", 230, "img.url", 233, 5);
+product.addProduct("juego de sabanas","color azul y blanco",150,"img.url",500,10);
+product.addProduct("remera", "color negro", 200, "img.url", 333, 5);
+product.addProduct("pantalon", "tipo jean color gris", 250, "img.url", 123, 3);
+product.addProduct("zapatillas", "color negro", 400, "img.url", 400, 200);
+product.addProduct("musculosa", "color negro", 250, "image.url", 489, 10);
 
-console.log("metodo getproducts que muestra el array de productos")
-console.log(product.getProducts());
-
-console.log("-------------------------")
-console.log("")
-console.log("muestro repeticion del code")
-product.addProduct("Producto 3" , "producto de prueba" , 400 ,"sin imagen" , "abc3" , 6);
-
-console.log("-----------------------")
-console.log("")
-
-console.log("muestro metodo getProducsById")
-console.log(product.getProductsById(1));
-console.log(product.getProductsById(2));
-console.log(product.getProductsById(3));
+ //product.saveProductsInFs(); Este meotodo guarda todos los productos en nuestro archivo json.
+ //product.deleteProduct(2); Este metodo elimina un producto de nuestro archivo dependiendo el id que le pasemos.
+ //product.updateProduct(2 , {price:200}); Este metodo actualiza alguna propiedad de nuestro prodcutos, recibe 2 parametro, el id del producto y la propiedad a actualizar(en forma de objeto).
+//product.getProductsById(2); Este metodo nos devuelve en forma de objeto js un producto de nuestro archivo JSON.
+//product.getProductsFs(); Este metodo nos devuelve en forma de array js, nuestro array alojado en nuestro archivo JSON.
